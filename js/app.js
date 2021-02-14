85a79a0acd9add652dcbaba819bd14bc158ae600
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-02-14 20:38:59 +0100

    navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+new Typewriter('#top-info', {
+  loop: true,
+  strings: ['Interaksjonsdesigner', 'UX-designer'],
+  autoStart: true,
+});
+
 function openlinkedin() {
 	window.open("https://www.linkedin.com/in/annamariabukalska/")
 
