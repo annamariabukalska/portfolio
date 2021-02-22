6cc1dd0c35ce1c133ffceed86b281655cdb23a93
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-02-22 21:52:10 +0100

    Forbedringer

--- js/app.js
+++ js/app.js
@@ -6,29 +6,12 @@ new Typewriter('#top-info', {
 
 function openlinkedin() {
 	window.open("https://www.linkedin.com/in/annamariabukalska/")
-
-	gtag('event', 'click', {
-		'event_category': 'outbound',
-		'event_label': "open-facebook",
-		'transport_type': 'beacon',
-	});
 }
 
 function openGitHub() {
 	window.open("https://github.com/annamariabukalska")
-
-	gtag('event', 'click', {
-		'event_category': 'outbound',
-		'event_label': "open-instagram",
-		'transport_type': 'beacon',
-	});
 }
 
 function emailAnna() {
 	window.open("mailto:annamariabukalska@gmail.com", '_self');
-	gtag('event', 'click', {
-		'event_category': 'outbound',
-		'event_label': "email-anna",
-		'transport_type': 'beacon',
-	});
 }
