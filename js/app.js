04ffe0a732bd7038ff9bb45cd4cdca5c5b1a9db7
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-02-25 09:33:40 +0100

    margin and prev next

--- js/app.js
+++ js/app.js
@@ -15,3 +15,39 @@ function openGitHub() {
 function emailAnna() {
 	window.open("mailto:annamariabukalska@gmail.com", '_self');
 }
+
+
+
+// attach event handlers with jquery
+$("#next").on("click", function (e) {
+  // check with following statement to see if event handler is attached properly
+  // alert("next");
+  next();
+});
+
+$("#prev").on("click", function (e) {
+  prev();
+});
+
+var images = new Array(
+  "http://placekitten.com/500/200",
+  "http://placekitten.com/499/200",
+  "http://placekitten.com/501/200",
+  "http://placekitten.com/500/199");
+
+function getCurrentImageIndex() {
+  return images.indexOf(document.getElementById("image").src);
+}
+
+function next() {
+  nextImage = (getCurrentImageIndex() + 1) % images.length;
+  document.getElementById("image").src = images[nextImage];
+  $("#textarea").val(images[nextImage]);
+}
+
+function prev() {
+  nextImage = (getCurrentImageIndex() - 1 + images.length) % images.length;
+  document.getElementById("image").src = images[nextImage];
+  $("#textarea").val(images[nextImage]);
+}
+
